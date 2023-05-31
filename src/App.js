91d7defe60cab94f55f873d1089d4131d3d30bb0
Lana Mihanovic <131782301+lanamihanovic@users.noreply.github.com>
2023-05-31 13:15:57 +0000

    Update App.js: r

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import "./App.css";
 import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Route></Route>
+        </Router>
       <Navbar />
     </div>
   );
