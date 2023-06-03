c97d6d25590eec8386a9937b712af7e96c572f73
Lana Mihanovic <131782301+lanamihanovic@users.noreply.github.com>
2023-06-03 14:23:39 +0000

    Update 13 files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import "./App.css";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
-import Naslovna from "./Naslovna";
+import Naslovna from "./Components/Naslovna";
+import Prva from "./Components/Prva";
+import Druga from "./Components/Druga";
+import Treca from "./Components/Treca";
+import Cetvrta from "./Components/Cetvrta";
+
+import React from "react";
+//import { render } from "react-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="/" element={<Naslovna />}>
-            {" Naslovna "}
-          </Route>
-        </Routes>
+        <div>
+          <ul>
+            <li>
+              <Link to="/"></Link>
+            </li>
+            <li>
+              <Link to="/Prva"></Link>
+            </li>
+            <li>
+              <Link to="/Druga">Topics</Link>
+            </li>
+            <li>
+              <Link to="/Treca"></Link>
+            </li>
+            <li>
+              <Link to="/Cetvrta"></Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={Naslovna} />
+          <Route path="/Prva" component={Prva} />
+          <Route path="/Druga" component={Druga} />
+          <Route path="/Treca" component={Treca} />
+          <Route path="/Cetvrta" component={Cetvrta} />
+        </div>
       </Router>
+      ;{" "}
     </div>
   );
 }
