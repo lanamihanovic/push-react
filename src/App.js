4270ebff794026048d2b52ad7e464c73fe32a586
Lana Mihanovic <131782301+lanamihanovic@users.noreply.github.com>
2023-06-04 18:27:20 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Naslovna from "./Components/Naslovna";
 import Prva from "./Components/Prva";
 import Druga from "./Components/Druga";
 import Treca from "./Components/Treca";
-import Cetvrta from "./Components/Cetvrta";
 
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
@@ -15,20 +14,18 @@ function App() {
         <div>
           <ul>
             <li>
-              <Link to="/">Naslovna</Link>
+              <Link to="/">Split</Link>
             </li>
             <li>
-              <Link to="/Prva">vijesti</Link>
+              <Link to="/Prva">Povijest </Link>
             </li>
             <li>
-              <Link to="/Druga">Vinyl</Link>
+              <Link to="/Druga">Račun</Link>
             </li>
             <li>
-              <Link to="/Treca">CD</Link>
+              <Link to="/Treca">Obavijesti</Link>
             </li>
-            <li>
-              <Link to="/Cetvrta">Prijava</Link>
-            </li>
+            <li></li>
           </ul>
 
           <hr />
@@ -37,12 +34,10 @@ function App() {
             <Route path="/Prva" component={Prva} />
             <Route path="/Druga" component={Druga} />
             <Route path="/Treca" component={Treca} />
-            <Route path="/Cetvrta" component={Cetvrta} />
           </Routes>
         </div>
       </Router>
       <Props />
-      
     </div>
   );
 }
--- src/Components/Cetvrta.js
+++
@@ -1,5 +0,0 @@
-function Cetvrta() {
-  return <div className="App"></div>;
-}
-
-export default Cetvrta;
